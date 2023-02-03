6acebf9a2d942fd95f976d429b0936f7cbcaa668
johnny2smooth <johnbp13@gmail.com>
2023-02-03 11:51:46 -0800

    tailwind added

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ async function getBooks() {
     database_id: process.env.NOTION_DATABASE_ID,
   });
 
+  // return response.results as QueryDatabaseResponse["results"];
   return response.results as QueryDatabaseResponse["results"];
 }
 
@@ -21,35 +22,36 @@ export default async function Home() {
   const books = await getBooks();
 
   return (
-    <main className={styles.main}>
-      <h1>Books</h1>
-      {books.map((book) => {
-        const { id, properties } = book as PageObjectResponse;
-        const { Rating, Review, Author, Created, Genre, Reviewer, Name } =
-          properties;
+    <main className={`${styles.main} `}>
+      <div className="border-2 border-slate-200 rounded-md grid p-6 gap-4">
+        {books.map((book) => {
+          const { id, properties, icon } = book as PageObjectResponse;
+          let { Rating, Review, Author, Genre, Reviewer, Name } = properties;
 
-        // I should be handling the types elsewhere, but this is just a quick example
+          let emoji = icon?.type === "emoji" ? icon.emoji : "📖";
 
-        return (
-          <div key={id}>
-            <h1>
-              {Name.type === "title" && Name.title[0].plain_text} by{" "}
-              {Author.type === "rich_text" && Author.rich_text[0].plain_text}
-            </h1>
-            <p>
-              A {Genre.type === "select" && Genre.select.name} book reviewed by{" "}
-              {Reviewer.type === "rich_text" &&
-                Reviewer.rich_text[0].plain_text}
-            </p>
-            <p>
-              {Review.type === "rich_text" && Review.rich_text[0].plain_text}
-            </p>
-            <p>
-              {Rating.type === "rich_text" && Rating.rich_text[0].plain_text}
-            </p>
-          </div>
-        );
-      })}
+          return (
+            <div key={id} className="border-2 border-slate-300 rounded">
+              <h1>
+                {emoji} {Name.type === "title" && Name.title[0].plain_text} by{" "}
+                {Author.type === "rich_text" && Author.rich_text[0].plain_text}
+              </h1>
+              <p>
+                A {Genre.type === "select" && Genre.select.name} book reviewed
+                by{" "}
+                {Reviewer.type === "rich_text" &&
+                  Reviewer.rich_text[0].plain_text}
+              </p>
+              <p>
+                {Review.type === "rich_text" && Review.rich_text[0].plain_text}
+              </p>
+              <p>
+                {Rating.type === "rich_text" && Rating.rich_text[0].plain_text}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
